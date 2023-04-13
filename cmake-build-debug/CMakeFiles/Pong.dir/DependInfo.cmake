c48faa2d706b276a405118142431f4743f0dde77
Steven Lopez <stevenlopez1416@gmail.com>
2023-04-13 15:15:02 -0700

    Second Commit

---
+++ cmake-build-debug/CMakeFiles/Pong.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/firehero1416/CLionProjects/Pong/Ball.cpp" "C:/Users/firehero1416/CLionProjects/Pong/cmake-build-debug/CMakeFiles/Pong.dir/Ball.cpp.obj"
+  "C:/Users/firehero1416/CLionProjects/Pong/BorderRect.cpp" "C:/Users/firehero1416/CLionProjects/Pong/cmake-build-debug/CMakeFiles/Pong.dir/BorderRect.cpp.obj"
+  "C:/Users/firehero1416/CLionProjects/Pong/Player.cpp" "C:/Users/firehero1416/CLionProjects/Pong/cmake-build-debug/CMakeFiles/Pong.dir/Player.cpp.obj"
+  "C:/Users/firehero1416/CLionProjects/Pong/ScoreCounter.cpp" "C:/Users/firehero1416/CLionProjects/Pong/cmake-build-debug/CMakeFiles/Pong.dir/ScoreCounter.cpp.obj"
+  "C:/Users/firehero1416/CLionProjects/Pong/main.cpp" "C:/Users/firehero1416/CLionProjects/Pong/cmake-build-debug/CMakeFiles/Pong.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML-2.5.1-windows-vc14-32-bit/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Pong.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong.dir/Ball.cpp.obj"
+  "CMakeFiles/Pong.dir/BorderRect.cpp.obj"
+  "CMakeFiles/Pong.dir/Player.cpp.obj"
+  "CMakeFiles/Pong.dir/ScoreCounter.cpp.obj"
+  "CMakeFiles/Pong.dir/main.cpp.obj"
+  "Pong.exe"
+  "Pong.exe.manifest"
+  "Pong.pdb"
+  "libPong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
